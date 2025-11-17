d2203d1aeb23182fe26d44a554bc65a593f18a93
Niharika Ibrahim <niharika8ibrahim@gmail.com>
2025-11-17 11:29:21 +0600

    third

--- labtask2.php
+++ labtask2.php
@@ -14,3 +14,45 @@
         color: red;
         font-size: 30px;
     }
+    table {
+        border-collapse: collapse;
+        margin-top: 20px;
+    }
+
+    td {
+        padding: 8px;
+        font-size: 18px;
+    }
+
+    input {
+        padding: 8px;
+        width: 250px;
+        border-radius: 5px;
+        border: 1px solid #aaa;
+    }
+
+    select {
+        padding: 8px;
+        width: 270px;
+        border-radius: 5px;
+    }
+    button {
+        background-color: red;
+        color: white;
+        border: none;
+        padding: 10px 15px;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 16px;
+    }
+
+    button:hover {
+        background-color: darkred;
+    }
+
+    hr {
+        border: none;
+        border-top: 3px solid red;
+        width: 60%;
+    }
+
